95e58be651446a32ea0975698dfe073724ce8348
Slime_hatena <git@aki-memo.net>
2021-02-01 03:00:21 +0900

    fix: デバッグ用のコードを削除

--- assets/ts/loading.ts
+++ assets/ts/loading.ts
@@ -1,7 +1,6 @@
 export async function loading() {
     let fonts = document.fonts;
     await fonts.load('10px "jf-dot-mplus10-subset"');
-    console.log("font");
 
     let loading = document.getElementById('loading');
     loading.classList.add('hide');
