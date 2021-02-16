b9c40851d60ea27008eebd6adab3de768551a8ff
Slime_hatena <git@aki-memo.net>
2021-02-17 01:13:45 +0900

    fix: jsファイルを1つに統合

--- assets/ts/loading.ts
+++
@@ -1,7 +0,0 @@
-export async function loading() {
-    let fonts = document.fonts;
-    await fonts.load('10px "jf-dot-mplus10-subset"');
-
-    let loading = document.getElementById('loading');
-    loading.classList.add('hide');
-}
